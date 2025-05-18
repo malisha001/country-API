bff4763790321a768e72fcd534f79896a307408a
malisha sandeepa <it22355232@my.sliit.lk>
2025-05-18 08:14:47 +0530

    fixed 3

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/country/', 
+  base: '/', 
   test: {
     globals: true,
     environment: 'jsdom',
